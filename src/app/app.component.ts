72b095a0b588878dd28111d0c657d55633031e22
Nemanja Fundup <nfundup@oddsandmore.com>
2024-03-27 13:55:05 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { User } from './user/user';
+import { UserService } from './user/user.service';
+import { response } from 'express';
+import { error } from 'console';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +13,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public users: User[] | undefined;
   title = 'FlightApp';
+
+  constructor(private userService: UserService){}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  public getUsers():void{
+    this.userService.getUsers().subscribe((response: User[])=>{
+      this.users=response;
+    },
+    (error: HttpErrorResponse)=>{
+      alert(error.message);
+    }
+    );
+  }
 }
--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -2,11 +2,12 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { User } from "./user";
+import { environment } from "../../environments/environment.development";
 
 @Injectable({ providedIn: 'root' })
 
 export class UserService {
-    private apiServerUrl = '';
+    private apiServerUrl = environment.apiBaseUrl;
 
     constructor(private http: HttpClient) { }
 
